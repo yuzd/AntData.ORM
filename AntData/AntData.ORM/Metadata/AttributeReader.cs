c62a45c19a67bf2ae6977127fef8af01e126d437
zdyu <zdyu@Ctrip.com>
2018-12-04 22:38:04 +0800

    修复 字段 被重写的时候 column属性读取不到的bug

--- AntData/AntData.ORM/Metadata/AttributeReader.cs
+++ AntData/AntData.ORM/Metadata/AttributeReader.cs
@@ -1,38 +1,54 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 using System.Reflection;
-
-using JetBrains.Annotations;
 
 namespace AntData.ORM.Metadata
 {
-	using Extensions;
+    using Extensions;
 
-	public class AttributeReader : IMetadataReader
-	{
-		[NotNull]
-		public T[] GetAttributes<T>(Type type, bool inherit = true)
-			where T : Attribute
-		{
-			var attrs = type.GetCustomAttributesEx(typeof(T), inherit);
-			var arr   = new T[attrs.Length];
+    public class AttributeReader : IMetadataReader
+    {
+        [NotNull]
+        public T[] GetAttributes<T>(Type type, bool inherit = true)
+            where T : Attribute
+        {
+            var attrs = type.GetCustomAttributesEx(typeof(T), inherit);
+            var arr = new T[attrs.Length];
 
-			for (var i = 0; i < attrs.Length; i++)
-				arr[i] = (T)attrs[i];
+            for (var i = 0; i < attrs.Length; i++)
+                arr[i] = (T)attrs[i];
 
-			return arr;
-		}
+            return arr;
+        }
 
-		[NotNull]
-		public T[] GetAttributes<T>(Type type, MemberInfo memberInfo, bool inherit = true)
-			where T : Attribute
-		{
-			var attrs = memberInfo.GetCustomAttributesEx(typeof(T), inherit);
-			var arr   = new T[attrs.Length];
+        [NotNull]
+        public T[] GetAttributes<T>(Type type, MemberInfo memberInfo, bool inherit = true)
+            where T : Attribute
+        {
+            var attrs = memberInfo.GetCustomAttributesEx(typeof(T), inherit);
+            if (attrs == null || attrs.Length < 1)
+            {
+                if (memberInfo.DeclaringType != null && memberInfo.DeclaringType.BaseType != null)
+                {
+                    if (memberInfo.IsFieldEx())
+                    {
+                        var newMenberInfo = memberInfo.DeclaringType.BaseType.GetField(memberInfo.Name);
+                        if (newMenberInfo != null) return GetAttributes<T>(type, newMenberInfo, inherit);
 
-			for (var i = 0; i < attrs.Length; i++)
-				arr[i] = (T)attrs[i];
+                    }
+                    else if (memberInfo.IsPropertyEx())
+                    {
+                        var newMenberInfo = memberInfo.DeclaringType.BaseType.GetProperty(memberInfo.Name);
+                        if (newMenberInfo != null) return GetAttributes<T>(type, newMenberInfo, inherit);
+                    }
+                }
+            }
+            var arr = new T[attrs.Length];
 
-			return arr;
-		}
-	}
+            for (var i = 0; i < attrs.Length; i++)
+                arr[i] = (T)attrs[i];
+
+            return arr;
+        }
+    }
 }
